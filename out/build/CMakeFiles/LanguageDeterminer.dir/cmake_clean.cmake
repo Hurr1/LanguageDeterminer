85f271297ffdd636f26c007e6530f46999e7ba96
hurr1 <egorpatenko@gmail.com>
2022-04-17 02:12:25 +0200

    Restruct

---
+++ out/build/CMakeFiles/LanguageDeterminer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LanguageDeterminer.dir/main.cpp.o"
+  "LanguageDeterminer"
+  "LanguageDeterminer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LanguageDeterminer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
